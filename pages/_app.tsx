2c8af7ae748085481c3a83b407b6c0af6c5734a8
sakamoto <sakamoto051eng@gmail.com>
2022-12-31 05:21:03 +0000

    fix: header disable at login/register

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,17 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Header } from '../components/organisms/Header'
 import { Footer } from '../components/organisms/Footer'
+import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const pathname = router.pathname;
   return (
     <>
-      <Header />
+      { pathname !== '/register' && 
+        pathname !== '/login' &&
+        <Header />
+      }
       <Component {...pageProps} />
       <Footer />
     </>
