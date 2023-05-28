99ffb316bb5b3a6521de3fbc9f7d3760ef2eece4
sakamoto051 <sakamoto051eng@gmail.com>
2023-05-28 06:26:03 +0000

    revert

--- functions/AxiosClientProvider.tsx
+++ functions/AxiosClientProvider.tsx
@@ -1,48 +1,34 @@
 import axios from 'axios';
 
 export const axiosGet = async (url: string) => {
-    await axios({
+    const res = await axios({
         method: 'get',
-        url: process.env.NEXT_PUBLIC_API_URL + '/sanctum/csrf-cookie',
+        url: process.env.NEXT_PUBLIC_API_URL + url,
         withCredentials: true,
     })
-        .then(async () => {
-            const res = await axios({
-                method: 'get',
-                url: process.env.NEXT_PUBLIC_API_URL + url,
-                withCredentials: true,
-            })
-                .then(res => {
-                    return res.data;
-                })
-                .catch(err => {
-                    console.log(err);
-                })
-            return res;
+        .then(res => {
+            return res.data;
         })
+        .catch(err => {
+            console.log(err);
+        })
+    return res;
 }
 
 export const axiosPost = async (url: string, data: Object) => {
-    await axios({
-        method: 'get',
-        url: process.env.NEXT_PUBLIC_API_URL + '/sanctum/csrf-cookie',
+    const res = await axios({
+        method: 'post',
+        url: process.env.NEXT_PUBLIC_API_URL + url,
+        data: data,
         withCredentials: true,
     })
-        .then(async () => {
-            const res = await axios({
-                method: 'post',
-                url: process.env.NEXT_PUBLIC_API_URL + url,
-                data: data,
-                withCredentials: true,
-            })
-                .then(res => {
-                    return res.data;
-                })
-                .catch(err => {
-                    console.log(err);
-                })
-            return res;
-        });
+        .then(res => {
+            return res.data;
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    return res;
 }
 
 export const axiosLogin = async (data: Object) => {
--- functions/CommonProvider.tsx
+++ functions/CommonProvider.tsx
@@ -2,23 +2,16 @@ import axios from "axios"
 import useSWR from 'swr';
 
 export const fetcher = async (url: string) => {
-    await axios({
-        method: 'get',
-        url: process.env.NEXT_PUBLIC_API_URL + '/sanctum/csrf-cookie',
+    const res = await axios(url, {
+        baseURL: process.env.NEXT_PUBLIC_API_URL,
         withCredentials: true,
     })
-        .then(async () => {
-            const res = await axios(url, {
-                baseURL: process.env.NEXT_PUBLIC_API_URL,
-                withCredentials: true,
-            })
-                .then(res => res.data)
-                .catch(err => {
-                    console.log(err);
-                    window.location.href = process.env.NEXT_PUBLIC_APP_URL + '/login';
-                });
-            return res;
-        });
+    .then(res => res.data)
+    .catch(err => {
+        console.log(err);
+        window.location.href = process.env.NEXT_PUBLIC_APP_URL + '/login';
+    });
+    return res;
 }
 
 export const getAuth = () => {
