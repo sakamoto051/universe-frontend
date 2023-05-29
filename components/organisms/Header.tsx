85cf2b5d5090f74353a4227549680f69190895f0
sakamoto051 <sakamoto051eng@gmail.com>
2023-05-29 05:04:25 +0000

    feat: post csrf

--- components/organisms/Header.tsx
+++ components/organisms/Header.tsx
@@ -3,16 +3,21 @@ import { Container } from '@mui/system';
 import { HomeButton } from '../atoms/Button/HomeButton';
 import { LogoutButton } from '../atoms/Button/LogoutButton';
 import useSWR from 'swr';
-import { fetcher } from '../../functions/CommonProvider';
+import { fetcher, getToken } from '../../functions/CommonProvider';
 import { BasicLoading } from '../atoms/Loading/BasicLoading';
 import { ThreadButton } from '../atoms/Button/ThreadButton';
+import Head from 'next/head';
 
 export const Header = () => {
     const { data } = useSWR('/api/user', fetcher);
     if (!data) return <BasicLoading />
+    const csrfToken = getToken();
 
     return (
         <Container sx={{ p: 2 }}>
+            <Head>
+                <meta name="csrf-token" content={csrfToken} />
+            </Head>
             <Stack spacing={2} direction='row'>
                 <HomeButton />
                 <ThreadButton />
